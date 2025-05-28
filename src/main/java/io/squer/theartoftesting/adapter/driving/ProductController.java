f558997a93f8e2787075ca3bae73d908e4cc72a0
Maik Basel <maik.basel@squer.io>
2025-05-28 08:45:54 +0200

    refactor: Do something with the error result

--- src/main/java/io/squer/theartoftesting/adapter/driving/ProductController.java
+++ src/main/java/io/squer/theartoftesting/adapter/driving/ProductController.java
@@ -2,6 +2,7 @@ package io.squer.theartoftesting.adapter.driving;
 
 import io.squer.theartoftesting.core.Product;
 import io.squer.theartoftesting.core.ProductService;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,8 @@ import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class ProductController {
+
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(ProductController.class);
 
     private final ProductService productService;
 
@@ -21,7 +24,9 @@ public class ProductController {
     public Product getProduct(@PathVariable String id) {
         var result = productService.getProduct(id);
 
+        LOG.error(result.getLeft().message());
+
         return result
-                .getOrElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, result.getLeft().message()));
+                .getOrElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
